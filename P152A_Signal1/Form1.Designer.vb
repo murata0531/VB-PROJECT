c25d0d490cdbe1d5e85144abc9e8a02f4dc50725
murata0531 <56902467+murata0531@users.noreply.github.com>
2021-02-20 02:16:42 +0900

    Add files via upload
    ok

---
+++ P152A_Signal1/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.StartButton = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(41, 24)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(64, 64)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'StartButton
+        '
+        Me.StartButton.Location = New System.Drawing.Point(36, 110)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(74, 26)
+        Me.StartButton.TabIndex = 1
+        Me.StartButton.Text = "StartButton"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(144, 157)
+        Me.Controls.Add(Me.StartButton)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "信号　1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents StartButton As Button
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ P152A_Signal1/Form1.vb
@@ -0,0 +1,47 @@
+﻿
+Public Class Form1
+
+    Dim SigImages(3) As Image
+    Dim SigIntervals() As Integer = {2000, 1000, 3000}
+    Dim ButtonTexts() As String = {"スタート", "ストップ"}
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        SigImages(0) = My.Resources.SignalG
+        SigImages(1) = My.Resources.SignalY
+        SigImages(2) = My.Resources.SignalR
+
+        PictureBox1.Image = SigImages(0)
+        Timer1.Interval = 2000
+        StartButton.Text = ButtonTexts(0)
+    End Sub
+
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+
+        Static s As Integer
+
+        If s = 0 Then
+            Timer1.Start()
+            s = 1
+        Else
+            Timer1.Stop()
+            s = 0
+        End If
+
+        StartButton.Text = ButtonTexts(s)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Static n As Integer
+
+        If n = 2 Then
+            n = 0
+        Else
+            n = n + 1
+        End If
+
+        PictureBox1.Image = SigImages(n)
+        Timer1.Interval = SigIntervals(n)
+    End Sub
+End Class
