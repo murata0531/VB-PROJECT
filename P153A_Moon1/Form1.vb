979d22a178cc4c20d5a22509db0916357a52bab1
murata0531 <56902467+murata0531@users.noreply.github.com>
2021-02-19 13:49:12 +0900

    Add files via upload
    ok

---
+++ P153A_Moon1/Form1.vb
@@ -0,0 +1,65 @@
+﻿
+Public Class Form1
+
+    Dim ButtonTexts() As String = {"スタート", "ストップ"}
+    Const BoxSize As Integer = 50
+    Const PanelWidth As Integer = 400
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.ClientSize = New Size(PanelWidth, 170)
+        Panel1.SetBounds(0, 0, PanelWidth, 120)
+
+        With PictureBox1
+            .SetBounds(-BoxSize, 30, BoxSize, BoxSize)
+            .Image = ImageList1.Images(0)
+        End With
+
+        With StartButton
+            .Location = New Point((PanelWidth - .Size.Width) \ 2, 130)
+            .Text = ButtonTexts(0)
+        End With
+
+    End Sub
+
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+
+        Static s As Integer
+
+        If s = 0 Then
+            Timer1.Start()
+            s = 1
+        Else
+            Timer1.Stop()
+            s = 0
+        End If
+
+        StartButton.Text = ButtonTexts(s)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+
+
+        With PictureBox1
+            .Location = New Point(.Location.X + 5, .Location.Y)
+
+            If .Location.X >= Me.ClientSize.Width Then
+
+                .Location = New Point(-BoxSize, .Location.Y)
+            End If
+
+            Static t As Integer
+
+            t += 1
+            If t = 90 Then
+                t = 0
+            End If
+
+            If t Mod 10 = 0 Then
+
+                PictureBox1.Image = ImageList1.Images(t \ 10)
+            End If
+        End With
+    End Sub
+End Class
