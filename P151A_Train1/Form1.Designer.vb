2a4182f22f0df43c9332e83aa59ddab9559aa0ac
murata0531 <56902467+murata0531@users.noreply.github.com>
2021-02-20 23:58:54 +0900

    Add files via upload
    ok

---
+++ P151A_Train1/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.StartButton = New System.Windows.Forms.Button()
+        Me.TrainLabel = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'StartButton
+        '
+        Me.StartButton.Location = New System.Drawing.Point(16, 16)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(72, 28)
+        Me.StartButton.TabIndex = 0
+        Me.StartButton.Text = "スタート"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'TrainLabel
+        '
+        Me.TrainLabel.Image = CType(resources.GetObject("TrainLabel.Image"), System.Drawing.Image)
+        Me.TrainLabel.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.TrainLabel.Location = New System.Drawing.Point(110, 16)
+        Me.TrainLabel.Name = "TrainLabel"
+        Me.TrainLabel.Size = New System.Drawing.Size(40, 60)
+        Me.TrainLabel.TabIndex = 1
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 50
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(178, 127)
+        Me.Controls.Add(Me.TrainLabel)
+        Me.Controls.Add(Me.StartButton)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "電車レース　１"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents StartButton As Button
+    Friend WithEvents TrainLabel As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ P151A_Train1/Form1.vb
@@ -0,0 +1,64 @@
+﻿
+Public Class Form1
+
+    Const StartStr As String = "スタート"
+    Const ResetStr As String = "リセット"
+    Const Last As Integer = 9
+    Dim TrainLabels(Last) As Label
+    Dim TrainRandom As Random = New Random
+    Dim LabelInitTop As Integer
+    Dim CopyDistance As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        LabelInitTop = TrainLabel.Location.Y
+        CopyDistance = TrainLabel.Size.Width + 12
+        With Me
+            .Size = New Size(.Size.Width + CopyDistance * 10, .Size.Height)
+            .MinimumSize = New Size(.Size.Width, 200)
+            .MaximumSize = New Size(.Size.Width, 400)
+        End With
+
+        TrainLabels(0) = TrainLabel
+
+        For i As Integer = 1 To 9
+
+            TrainLabels(i) = New Label
+            With TrainLabels(i)
+                .Location = New Point(TrainLabels(i - 1).Location.X + CopyDistance, LabelInitTop)
+                .Size = TrainLabel.Size
+                .Image = TrainLabel.Image
+                .ImageAlign = TrainLabel.ImageAlign
+            End With
+
+            Me.Controls.AddRange(TrainLabels)
+
+            StartButton.Text = StartStr
+        Next
+    End Sub
+
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+
+        If StartButton.Text = StartStr Then
+            StartButton.Text = ResetStr
+            Timer1.Start()
+
+        Else
+
+            StartButton.Text = StartStr
+            Timer1.Stop()
+            For Each tLabel As Label In TrainLabels
+                tLabel.Location = New Point(tLabel.Location.X, LabelInitTop)
+            Next
+            Me.Size = Me.MinimumSize
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Dim n As Integer = TrainRandom.Next(0, 10)
+
+        With TrainLabels(n)
+            .Location = New Point(.Location.X, .Location.Y + 5)
+        End With
+    End Sub
+End Class
